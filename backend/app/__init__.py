29a7e3855fcf0c7f21a77854b07bd553730ed8d1
tajindersigh111 <tajindsingh012@gmail.com>
2026-08-19 23:50:20 +0530

    fix(backend): check if argument is a class in Enum monkeypatch to allow loading literal migrations

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,7 +11,7 @@ original_enum_init = types.Enum.__init__
 
 
 def _patched_enum_init(self, *enums, **kw):
-    if enums and issubclass(enums[0], enum.Enum) and "values_callable" not in kw:
+    if enums and isinstance(enums[0], type) and issubclass(enums[0], enum.Enum) and "values_callable" not in kw:
         kw["values_callable"] = lambda x: [e.value for e in x]
     original_enum_init(self, *enums, **kw)
 
